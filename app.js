540a65d44b608141e9fd78ecfb3cfdab56d9e314
John Manack <jmnck28@gmail.com>
2021-01-28 15:07:51 -0500

    establishes express server

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const HTTP = require('http');
+
+const HOSTNAME = '127.0.0.1',
+    PORT = 3000;
+
+const express = require('express'),
+    es6Renderer = require('express-es6-template-engine');
+
+const app = express();
+
+app.engine('html', es6Renderer);
+app.set('view', 'templates');
+app.set('view engine', 'html');
+
+const SERVER = HTTP.createServer(app);
+
+SERVER.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}`)
+});
+
